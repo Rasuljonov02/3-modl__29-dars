0dc1a9fa7ad9acd91d1089f6ef2207dd1441cba4
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-25 15:33:50 +0500

    kerakli

--- 29-dars/drag.js
+++ 29-dars/drag.js
@@ -1,48 +1,48 @@
-const draggables = document.querySelectorAll(".task");
-const droppables = document.querySelectorAll(".todo");
+const draggables = document.querySelectorAll(".task"); // kichkina divcha
+const droppables = document.querySelectorAll(".todo"); //katta div
 
 draggables.forEach((task) => {
-  task.addEventListener("dragstart", () => {
-    task.classList.add("is-dragging");
-    console.log('drag start');
-  });
-  task.addEventListener("dragend", () => {
-    task.classList.remove("is-dragging");
-    console.log('drag end');
-  });
+	task.addEventListener("dragstart", () => {
+		task.classList.add("is-dragging");
+		console.log("drag start");
+	});
+	task.addEventListener("dragend", () => {
+		task.classList.remove("is-dragging");
+		console.log("drag end");
+	});
 });
 
 droppables.forEach((zone) => {
-  zone.addEventListener("dragover", (e) => {
-    e.preventDefault();
+	zone.addEventListener("dragover", (e) => {
+		e.preventDefault();
 
-    const bottomTask = insertAboveTask(zone, e.clientY);
-    const curTask = document.querySelector(".is-dragging");
+		const bottomTask = insertAboveTask(zone, e.clientY);
+		const curTask = document.querySelector(".is-dragging");
 
-    if (!bottomTask) {
-      zone.appendChild(curTask);
-    } else {
-      zone.insertBefore(curTask, bottomTask);
-    }
-  });
+		if (!bottomTask) {
+			zone.appendChild(curTask);
+		} else {
+			zone.insertBefore(curTask, bottomTask);
+		}
+	});
 });
 
 const insertAboveTask = (zone, mouseY) => {
-  const els = zone.querySelectorAll(".task:not(.is-dragging)");
+	const els = zone.querySelectorAll(".task:not(.is-dragging)");
 
-  let closestTask = null;
-  let closestOffset = Number.NEGATIVE_INFINITY;
+	let closestTask = null;
+	let closestOffset = Number.NEGATIVE_INFINITY;
 
-  els.forEach((task) => {
-    const { top } = task.getBoundingClientRect();
+	els.forEach((task) => {
+		const { top } = task.getBoundingClientRect();
 
-    const offset = mouseY - top;
+		const offset = mouseY - top;
 
-    if (offset < 0 && offset > closestOffset) {
-      closestOffset = offset;
-      closestTask = task;
-    }
-  });
+		if (offset < 0 && offset > closestOffset) {
+			closestOffset = offset;
+			closestTask = task;
+		}
+	});
 
-  return closestTask;
+	return closestTask;
 };
